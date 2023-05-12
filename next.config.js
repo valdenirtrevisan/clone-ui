d72f9e4ae8131284b936350240a488239e523c3b
Valdenir Trevisan Junior <valdenirtrevi@gmail.com>
2023-05-12 14:39:34 -0300

    Initialize feature/tailwind

---
+++ next.config.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-undef */
+/** @type {import('next').NextConfig} */
+module.exports = {
+  /* config options here */
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Page with tailwind css</h1>
+    </div>
+  );
+};
+
+export default Page;
